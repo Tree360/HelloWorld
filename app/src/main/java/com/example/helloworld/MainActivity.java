82c893a44ee74b1fc3776e45edca153359d3620a
Tree360 <tbonkoski@wisc.edu>
2021-09-20 11:50:35 -0500

    Imageview fix

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.helloworld;
 
+import android.os.Bundle;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     ImageView imgview;
